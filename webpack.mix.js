743dd4f56a872c7a40354d22dca70b47462df611
gorbunov <v.gorbunov@studiofact.ru>
2022-11-20 16:38:06 +0500

    Шаблон AdminLTE. Part 2.

--- webpack.mix.js
+++ webpack.mix.js
@@ -27,3 +27,5 @@ mix.copyDirectory('resources/assets/admin/plugins/fontawesome-free/webfonts', 'p
 mix.copyDirectory('resources/assets/admin/img', 'public/assets/admin/img');
 
 mix.copy( 'resources/assets/admin/css/adminlte.min.css.map', 'public/assets/admin/css/adminlte.min.css.map')
+
+mix.copy( 'resources/assets/admin/js/adminlte.min.js.map', 'public/assets/admin/js/adminlte.min.js.map')
