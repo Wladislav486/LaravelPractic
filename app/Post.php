7e3c839a86a5e79ced4253c29a2b10569e2497af
gorbunov <vladgorbunov486@gmail.com>
2022-11-20 19:02:54 +0500

    Отношения моделей.

--- app/Post.php
+++ app/Post.php
@@ -3,8 +3,31 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Cviebrock\EloquentSluggable\Sluggable;
 
 class Post extends Model
 {
-    //
+    use Sluggable;
+
+    public function tags(): BelongsToMany
+    {
+        return $this->belongsToMany(Tag::class);
+    }
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
+    }
+
 }
