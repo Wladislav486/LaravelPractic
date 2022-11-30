0b3c02cf4422b6bf0236ffdf7eb1d8b4938a0570
gorbunov <vladgorbunov486@gmail.com>
2022-11-30 22:53:24 +0500

    Статьи категорий. Статьи по тегу.

---
+++ app/Http/Controllers/TagController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Tag;
+
+class TagController extends Controller
+{
+    public function show($slug)
+    {
+        $tag = Tag::where('slug', $slug)->firstOrFail();
+        $posts = $tag
+            ->posts()
+            ->with('category')
+            ->orderBy('id', 'desc')
+            ->paginate(2);
+        return view('tags.show', compact('posts', 'tag'));
+    }
+}
